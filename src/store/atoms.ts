7a1834b85b651e7eab75b5ef08ec0d3488dea324
rydawnshootingstar <rydawnshootingstar@gmail.com>
2024-08-26 19:23:40 -0400

    remove unused jotai experimentation

--- src/store/atoms.ts
+++
@@ -1,4 +0,0 @@
-import { Pet } from '@/lib/types';
-import { atom } from 'jotai';
-
-export const activePet = atom<Pet | unknown>({ id: '', name: '', ownerName: '', imageUrl: '', age: null, notes: '' });
